5977d5f50461a87b75ce8c0ff6c014f7f8eaad93
iampaulanca <pancajima@mail.sfsu.edu>
2019-07-14 23:49:58 -0700

    Code Deploy is working again, fixed redirect issue

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
 from PortfolioWebsite import app
-from flask import request, redirect
 from waitress import serve
-
-# @app.before_request
-# def before_request():
-#     if not request.is_secure and app.env != "development":
-#         print(app.env)
-#         url = request.url.replace("http://", "https://", 1)
-#         code = 301
-#         return redirect(url, code=code)
 
 if __name__ == "__main__":
     serve(app, listen='0.0.0.0:80', url_scheme='https')
-    # app.run(host='0.0.0.0', port='5000', debug=True)
 
