9e916b0726a89485a5983dd28966f971d192f567
iampaulanca <pancajima@mail.sfsu.edu>
2019-07-15 13:53:18 -0700

    added useful text and fixed some CSS issues

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from PortfolioWebsite import app
 from waitress import serve
 
 if __name__ == "__main__":
-    serve(app, listen='0.0.0.0:80', url_scheme='https')
+    # serve(app, listen='0.0.0.0:80', url_scheme='https')
+    serve(app, listen='0.0.0.0:5000')
 
