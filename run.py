f953f2788ef3bf332dc694b6f533a6583ded6760
iampaulanca <pancajima@mail.sfsu.edu>
2019-07-19 00:36:06 -0700

    Requirements.txt went missin?

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from PortfolioWebsite import app
 from waitress import serve
 
-
 if __name__ == "__main__":
 
-    serve(app, listen='0.0.0.0:80', url_scheme='https')
-    # app.run(host='0.0.0.0', port=5000, debug=True)
+    # serve(app, listen='0.0.0.0:80', url_scheme='https')
+    app.run(host='0.0.0.0', port=5000, debug=True)
