58efd95a71b5d8451512ad47c03a5eac28916244
iampaulanca <pancajima@mail.sfsu.edu>
2019-07-18 18:51:37 -0700

    changed listening port

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from waitress import serve
 
 if __name__ == "__main__":
 
-    # serve(app, listen='0.0.0.0:80', url_scheme='https')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    serve(app, listen='0.0.0.0:80', url_scheme='https')
+    # app.run(host='0.0.0.0', port=5000, debug=True)
