977a7f9904f629df67596afa544b15c3f3e1a3b6
iampaulanca <pancajima@mail.sfsu.edu>
2019-07-20 22:19:30 -0700

    create, update, delete post complete and using mysql now

--- PortfolioWebsite/routes.py
+++ PortfolioWebsite/routes.py
@@ -85,6 +85,18 @@ def update_post(post_id):
     return render_template("create_post.html", title="Update Post", post_form=post_form, legend='Update Post')
 
 
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post has been deleted!', 'success')
+    return redirect(url_for('blog'))
+
+
 @app.route('/blog/new', methods=['GET', 'POST'])
 @login_required
 def new_post():
