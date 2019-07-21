401888d2f979816a26391d91b3b5653669513df0
iampaulanca <pancajima@mail.sfsu.edu>
2019-07-20 21:09:28 -0700

    create, update, delete post routes

--- PortfolioWebsite/__init__.py
+++ PortfolioWebsite/__init__.py
@@ -2,14 +2,23 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+import os
 from datetime import timedelta
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'TEMP'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db?check_same_thread=False'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+# Flask-Migrate https://www.youtube.com/watch?v=BAOfjPuVby0&t=29s
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'blog'
 login_manager.login_message_category = 'info'
--- PortfolioWebsite/models.py
+++ PortfolioWebsite/models.py
@@ -3,6 +3,7 @@ from PortfolioWebsite import db, login_manager
 from flask_login import UserMixin
 
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -23,6 +24,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    subtitle = db.Column(db.String(100), nullable=True)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
